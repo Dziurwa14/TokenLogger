850f2c26103b0ba0c8eaefbd5909987d92b564cc
Dziurwa14 <agev@o2.pl>
2023-09-15 22:11:24 +0200

    cutely steal code

--- plugins/customVoiceMessages/src/patches/download.tsx
+++ plugins/customVoiceMessages/src/patches/download.tsx
@@ -1,35 +1,45 @@
 import { before, after } from "@vendetta/patcher"
 import { getAssetIDByName as getAssetId } from "@vendetta/ui/assets"
 import { findByProps } from "@vendetta/metro"
-import { React } from "@vendetta/metro/common"
+import { findInReactTree } from "@vendetta/utils";
+import { React, clipboard } from "@vendetta/metro/common"
 import { Forms } from "@vendetta/ui/components"
+import CoolRow from "../components/CoolRow";
 
 const ActionSheet = findByProps("openLazy", "hideActionSheet")
-const Icon = Forms.FormIcon
-const { FormRow } = Forms
 
 export default () => before("openLazy", ActionSheet, (ctx) => {
     const [component, args, actionMessage] = ctx
-    if (args !== "MessageLongPressActionSheet") return
+    const message = actionMessage?.message;
+    if (args !== "MessageLongPressActionSheet" || !message) return;
     component.then(instance => {
         const unpatch = after("default", instance, (_, component) => {
             React.useEffect(() => () => { unpatch() }, [])
-            let [msgProps, buttons] = component.props?.children?.props?.children?.props?.children
-
-            const message = msgProps?.props?.message ?? actionMessage?.message
-
-            if (!buttons || !message) return
+            const buttons = findInReactTree(
+                component,
+                (x) => x?.[0]?.type?.name === "ButtonRow"
+            );
+            if (!buttons) return component;
 
             if (message.hasFlag(8192))
                 buttons.splice(5, 0,
-                    <FormRow
+                    <CoolRow
                         label="Download Voice Message"
-                        leading={<Icon source={getAssetId("ic_download_24px")} />}
+                        icon={getAssetId("ic_download_24px")}
                         onPress={async () => {
                             await findByProps("downloadMediaAsset").downloadMediaAsset(message.attachments[0].url, 0)
                             findByProps("hideActionSheet").hideActionSheet()
                         }}
                     />)
+            buttons.splice(6, 0,
+                <CoolRow
+                    label="Copy Voice Message URL"
+                    icon={getAssetId("copy")}
+                    onPress={async () => {
+                        clipboard.setString(message.attachments[0].url)
+                        findByProps("hideActionSheet").hideActionSheet()
+                    }}
+                />)
         })
     })
 })
